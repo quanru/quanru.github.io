b2c83dadb479918eb427741fbadd67216a09ddea
linyibing <linyibing@bytedance.com>
2024-02-02 20:38:26 +0800

    feat: add microsoft clarity

--- scripts/inject.js
+++ scripts/inject.js
@@ -6,6 +6,17 @@ hexo.extend.injector.register('head_end', () => {
 });
 
 hexo.extend.injector.register(
+  'head_end',
+  `<script type="text/javascript">
+    (function(c,l,a,r,i,t,y){
+        c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
+        t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
+        y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
+    })(window, document, "clarity", "script", "kvbvszxxym");
+  </script>`
+);
+
+hexo.extend.injector.register(
   'body_end',
   `<script src="https://giscus.app/client.js"
         data-repo="quanru/quanru.github.io"
